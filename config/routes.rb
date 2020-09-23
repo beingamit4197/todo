a78c7a7a0c621ddda7e203ae33297506b5c28e11
Amit Rawat <beingamit4197@gmail.com>
2020-09-23 13:32:34 +0530

    auth to Facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :todo_lists do
     resources :todo_items do
       member do
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
 
  # root 'todo_lists#index'
  root 'user#home'
+
+ devise_scope :user do
+  get 'user/sign_in', to: 'user/sessions#new', as: :new_user_session_path
+  get 'user/sign_out', to: 'user/sessions#destroy', as: :destroy_user_session_path
+end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
